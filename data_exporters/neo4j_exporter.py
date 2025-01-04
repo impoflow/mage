eca88c01ba0afdc77f82abab15620e15bf5fae38
orr21 <oscar.rico101@alu.ulpgc.es>
2025-01-04 23:11:50 +0000

    fix neo4j

--- data_exporters/neo4j_exporter.py
+++ data_exporters/neo4j_exporter.py
@@ -1,20 +1,28 @@
-if 'data_exporter' not in globals():
-    from mage_ai.data_preparation.decorators import data_exporter
+from os import path
 
-import pandas as pd
+from pandas import DataFrame
+
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+
 from mage.utils.db_connection import Neo4jConnection
 from mage.utils.db_handler import Neo4jHandler
 import neo4j
-import os
 
-from mage_ai.data_preparation.shared.secrets import get_secret_value
-
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
 
 @data_exporter
-def export_data(data, *args, **kwargs):
-    uri = f"bolt://{os.environ.get('NEO_HOST')}:7687"
-    user = os.environ.get('NEO_USER')
-    password = os.environ.get('NEO_PASSWD')
+def export_data(df: DataFrame, **kwargs):
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+    
+    config_loader = ConfigFileLoader(config_path, config_profile)
+    neo4j_config = config_loader.config
+
+    uri = neo4j_config.get('NEO_CONNECTION_STRING')
+    user = neo4j_config.get('NEO_USER')
+    password = neo4j_config.get('NEO_PASSWD')
 
     neo4j_connection = Neo4jConnection(uri, user, password)
     neo4j_connection.connect()
@@ -24,7 +32,7 @@ def export_data(data, *args, **kwargs):
     try:
         handler.add_user(kwargs['user'])
         handler.add_project(kwargs['project_name'], kwargs['user'])
-        handler.add_classes_from_csv(kwargs['user'], kwargs['project_name'], data)
+        handler.add_classes_from_csv(kwargs['user'], kwargs['project_name'], df)
         handler.create_ownership(kwargs['user'], kwargs['project_name'])
 
         for user in kwargs['collaborators']:
